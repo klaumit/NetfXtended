fdc2f9dae0456103456e708b9c1de73f48183a33
Klaus Mittermann <klaus.mittermann@hotmail.com>
2025-06-18 20:38:22 +0200

    Initial code

---
+++ src/NetfXtended.Tests/PathTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace NetfXtended.Tests
+{
+    public class PathTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Fail("Not good!");
+        }
+    }
+}
