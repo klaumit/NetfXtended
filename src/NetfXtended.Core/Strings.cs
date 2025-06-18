6de93d20495dc731f68145c76f4b17b885ce296f
Klaus Mittermann <klaus.mittermann@hotmail.com>
2025-06-18 22:17:58 +0200

    Create Strings.cs

---
+++ src/NetfXtended.Core/Strings.cs
@@ -0,0 +1,19 @@
+﻿namespace NetfXtended.Core
+{
+    public static class Strings
+    {
+        public static bool IsNullOrWhiteSpace(string text)
+        {
+#if NETFRAMEWORK
+            if (text == null)
+                return true;
+            text = text.Trim();
+            if (text.Length == 0)
+                return true;
+            return false;
+#else
+            return string.IsNullOrWhiteSpace(text);
+#endif
+        }
+    }
+}
