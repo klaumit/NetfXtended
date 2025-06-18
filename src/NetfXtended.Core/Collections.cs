cd80e6626130f4c59418deff69fc21927e6532ed
Klaus Mittermann <klaus.mittermann@hotmail.com>
2025-06-18 22:03:50 +0200

    Adding code

---
+++ src/NetfXtended.Core/Collections.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace NetfXtended.Core
+{
+    public static class Collections
+    {
+        public static void AddRange<T>(this ICollection<T> coll, IEnumerable<T> items)
+        {
+            foreach (var item in items)
+                coll.Add(item);
+        }
+    }
+}
---
+++ src/NetfXtended.Core/Files.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace NetfXtended.Core
+{
+    public static class Files
+    {
+        public static IEnumerable<string> ReadLines(string file, Encoding enc)
+        {
+#if NET20 || NET35
+            return File.ReadAllLines(file, enc);
+#else
+            return File.ReadLines(file, enc);
+#endif
+        }
+    }
+}
