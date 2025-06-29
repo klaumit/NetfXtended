1b7ee49df869d3e48d7503069253d9499b26bab6
Klaus Mittermann <klaus.mittermann@hotmail.com>
2025-06-29 15:18:07 +0200

    Optimizing code

---
+++ src/NetfXtended.Tests/ValueTests.cs
@@ -0,0 +1,19 @@
+﻿using NetfXtended.Core;
+using NUnit.Framework;
+using NetfXtended.WinForms;
+using NUnit.Framework;
+
+#pragma warning disable NUnit2005
+
+namespace NetfXtended.Tests
+{
+    public class ValueTests
+    {
+        [Test]
+        public void Check()
+        {
+            Assert.AreEqual(-9229841, Values.ParseHexS("FF7329EF"));
+            Assert.AreEqual(4285737455, Values.ParseHexU("FF7329EF"));
+        }
+    }
+}
