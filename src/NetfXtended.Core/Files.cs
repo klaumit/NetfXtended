4f53496e8426fdb3b0be52a83a9565360756da1c
Klaus Mittermann <klaus.mittermann@hotmail.com>
2025-06-29 15:58:20 +0200

    Adding file reading helper

--- src/NetfXtended.Core/Files.cs
+++ src/NetfXtended.Core/Files.cs
@@ -1,17 +1,30 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace NetfXtended.Core
 {
     public static class Files
     {
-        public static IEnumerable<string> ReadLines(string file, Encoding enc)
+        public static IEnumerable<string> ReadLines(string file, Encoding enc = null)
         {
+            var encT = enc ?? Encoding.UTF8;
 #if NET20 || NET35
-            return File.ReadAllLines(file, enc);
+            return File.ReadAllLines(file, encT);
 #else
-            return File.ReadLines(file, enc);
+            return File.ReadLines(file, encT);
+#endif
+        }
+
+        public static void WriteLines(string file, IEnumerable<string> lines, Encoding enc = null)
+        {
+            var encT = enc ?? Encoding.UTF8;
+#if NET20 || NET35
+            var array = lines.ToArray();
+            File.WriteAllLines(file, array, encT);
+#else
+            File.WriteAllLines(file, lines, encT);
 #endif
         }
     }
---
+++ src/NetfXtended.Tests/FileTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using NetfXtended.Core;
+using NUnit.Framework;
+
+#pragma warning disable NUnit2005
+
+namespace NetfXtended.Tests
+{
+    public class FileTests
+    {
+        [Test]
+        public void Check()
+        {
+            const string file = "files.txt";
+
+            var inputs = new List<string> { "'Hello','World!'" };
+            Files.WriteLines(file, inputs);
+
+            var outputs = Files.ReadLines(file);
+            Assert.AreEqual(inputs, outputs);
+        }
+    }
+}
