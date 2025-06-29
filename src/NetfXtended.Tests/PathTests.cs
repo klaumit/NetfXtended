b2a29cad22fbccf972828c176973b0edec4b2aa8
Klaus Mittermann <klaus.mittermann@hotmail.com>
2025-06-29 16:27:06 +0200

    Adding path test

---
+++ src/NetfXtended.Tests/PathTests.cs
@@ -0,0 +1,17 @@
+﻿using NetfXtended.Core;
+using NUnit.Framework;
+
+#pragma warning disable NUnit2005
+
+namespace NetfXtended.Tests
+{
+    public class PathTests
+    {
+        [Test]
+        public void CheckProject()
+        {
+            var path = Paths.GetProjectPath(typeof(Resources));
+            Assert.IsTrue(path.Contains(@"\bin\"));
+        }
+    }
+}
