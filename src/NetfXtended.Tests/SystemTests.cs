5a9c1a9ddf3c1ae22abdbee2d1d60e10648e205b
Klaus Mittermann <klaus.mittermann@hotmail.com>
2025-06-29 16:50:16 +0200

    Adding last test so far

---
+++ src/NetfXtended.Tests/SystemTests.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using NetfXtended.Core;
+using NUnit.Framework;
+
+#pragma warning disable NUnit2005
+
+namespace NetfXtended.Tests
+{
+    public class SystemTests
+    {
+        [Test]
+        public void CheckTmpFile()
+        {
+            var path = Systems.GetTmpFile("json");
+            Assert.IsTrue(path.Contains(@"\AppData\Local\Temp\"));
+        }
+
+        [Test]
+        public void CheckOpen()
+        {
+            Assert.Throws<Win32Exception>(() => Systems.Open("test.json"));
+        }
+    }
+}
